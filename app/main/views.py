477fffdc289a53646b03410e862d9798bf98cf04
mugala <mugalalevi21@gmail.com>
2018-04-22 10:46:28 +0300

    adding routes This commit will add the four routes for the index about posts and contacts

--- app/main/views.py
+++ app/main/views.py
@@ -2,5 +2,20 @@ from . import main
 
 @main.route('/')
 def index():
-    return '<h1> Hello World </h1>'
+    return render_template ('index.html')
+  
 
+
+@main.route('/about')
+def about():
+    return render_template ('about.html')
+
+
+@main.route('/post')
+def post():
+    return render_template ('post.html')
+
+
+@main.route('/contact')
+def contact():
+    return render_template ('contact.html')
