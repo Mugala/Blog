9f3856e2eb58f69ce6d35985c05af446a036f4ba
mugala <mugalalevi21@gmail.com>
2018-04-23 10:22:25 +0300

    Database This commit will add the tables to the database

--- app/models.py
+++ app/models.py
@@ -1,15 +1,102 @@
-from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import db,admin
+from . import login_manager
+from flask_login import UserMixin
+from datetime import datetime
+from flask_admin.contrib.sqla import ModelView
+from flask_admin import Admin
 
-#...
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(200),unique=True)
+    username=db.Column(db.String(200),unique=True)
+    password_hash = db.Column(db.String(200))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+    def security_context_processor():
+            return dict(
+            admin_base_template=admin.base_template,
+            admin_view=admin.index_view,
+            h=admin_helpers,
+            )
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+class Posts(db.Model):
+    __tablename__='posts'
+
+    id = db.Column(db.Integer, primary_key =True)
+    title = db.Column(db.String(200))
+    content = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_post(self):
+        db.session.add(self)
+        db.session.commit()
+
 
     def __repr__(self):
         return f'User {self.username}'
 
+class Comments(db.Model):
+    __tablename__='comments'
+
+    id=db.Column(db.Integer, primary_key=True)
+    comment= db.Column(db.String(200))
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    posts_id = db.Column(db.Integer,db.ForeignKey("posts.id"))
+
+    # def save_comment(self):
+    #     db.session.add(self)
+    #     db.session.commit()
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Subscription(db.Model):
+
+    __tablename__='subscription'
+
+    id=db.Column(db.Integer,primary_key=True)
+    email=db.Column(db.String(200))
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
 
 
 
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Posts, db.session))
+admin.add_view(ModelView(Comments, db.session))
