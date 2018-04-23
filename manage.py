8fa19126bd68b6ae632fb4899cbb3e7e76366fe3
mugala <mugalalevi21@gmail.com>
2018-04-23 09:45:17 +0300

    Flash script shell This commit allows us to add FLask script shell allows us to create a python shell inside our app

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 
+
 from flask_script import Manager,Server
+from app.models import User
 from app import create_app,db
 
 app = create_app('default')
@@ -8,6 +10,8 @@ manager = Manager(app)
 
 manager.add_command('server', Server)
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 if __name__ == '__main__':
     manager.run()
-
