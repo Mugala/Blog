599cd23dd19efb3b2c4a0f5b834bd46ebadc1c62
mugala <mugalalevi21@gmail.com>
2018-04-23 10:02:04 +0300

    this commit will import os in to the config.py

--- app/models.py
+++ app/models.py
@@ -11,17 +11,5 @@ class User(db.Model):
         return f'User {self.username}'
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 
-
-class User(UserMixin,db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(200),unique=True)
-    username=db.Column(db.String(200),unique=True)
-    password_hash = db.Column(db.String(200))
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
--- config.py
+++ config.py
@@ -1,11 +1,24 @@
+
+import os
+
 class Config:
     pass
-
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
 class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://levi:eyeshadow@localhost/Personal_Blog'
 
-config_options ={"production":ProdConfig,"default":DevConfig}
-
+config_options ={"production":ProdConfig,"default":DevConfig}   
