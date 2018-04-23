5598368a828bcacf413ccc2a8c3572514145f4b9
mugala <mugalalevi21@gmail.com>
2018-04-23 10:27:56 +0300

    auth folder This commit will add the auth folder structure to the app directory

--- manage.py
+++ manage.py
@@ -1,17 +1,22 @@
 
-
-from flask_script import Manager,Server
-from app.models import User
+from flask_script import Manager,Shell,Server
 from app import create_app,db
+from flask_migrate import Migrate, MigrateCommand
+from app.models import User,Role,Posts,Comments,Subscription
+from flask_admin.contrib.sqla import ModelView
+from flask_admin import Admin
 
 app = create_app('default')
 
 manager = Manager(app)
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 manager.add_command('server', Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app, db=db, User=User, Role=Role, Admin=Admin, Subscription=Subscription)
+
 if __name__ == '__main__':
     manager.run()
